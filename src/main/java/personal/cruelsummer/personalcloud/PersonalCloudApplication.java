8022e0b1d4cbf18cfdf7a344f57d21c97fe31aa6
CruelSummer <linwush877003@outlook.com>
2023-05-30 14:24:03 +0800

    init project

---
+++ src/main/java/personal/cruelsummer/personalcloud/PersonalCloudApplication.java
@@ -0,0 +1,13 @@
+package personal.cruelsummer.personalcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/personal/cruelsummer/personalcloud/PersonalCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.cruelsummer.personalcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
